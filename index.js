17a27ee41769fc13b61ace60cc9d83292694bc1c
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2019-12-25 23:41:13 +0900

    1225
    1225

---
+++ index.js
@@ -0,0 +1,15 @@
+//let a = 221;
+//let b = a - 5;
+//a = 4
+//console.log(b, 4); //결과는 216 , 4 가 나온다
+
+// const a = 221;
+// const b = a - 5;
+// console.log(b, a);
+//const는 constance 변하지 않는 상수를 이야기함
+//변수를 바꾸면 안됨 const, 바꿔도됨 let
+//옛날에는 var만 있어서 불편해씀
+
+var a = 221;
+var b = a - 5;
+console.log(b, a);
---
+++ 기본.js
@@ -0,0 +1,8 @@
+function numbering(){
+    i = 0;
+    while(i < 10){
+        document.write(i);
+        i += 1;
+    }   
+}
+numbering();
