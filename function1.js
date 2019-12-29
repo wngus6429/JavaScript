62b9a2344adf73367c73c920dd99adbb399d052e
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2019-12-30 01:06:47 +0900

    1230
    1230

---
+++ function1.js
@@ -0,0 +1,11 @@
+// function sayHello(name, age) {
+//   console.log("hello!", name, age );
+// }
+
+// sayHello("nicolas", 15);
+
+function sayHello(name, age) {
+  console.log(`Hello ${name} you are ${age} years old`);
+}
+
+sayHello()
